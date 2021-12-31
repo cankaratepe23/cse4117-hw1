e8514a3572fbce493bd6e870330763923f0f7a98
Eymen Topçuoğlu <eymentopcuoglu52@gmail.com>
2021-12-31 18:15:45 +0300

    Add boilerplate verilog for modified bird

--- cpu-verilog/main.v
+++ cpu-verilog/main.v
@@ -1,43 +1,85 @@
-module main (grounds, display, clk, pushbutton,led);
+//Main module
+
+
+module main_module (
+			output wire [3:0] rowwrite,
+			input [3:0] colread,
+			input clk,
+			output wire [3:0] grounds,
+			output wire [6:0] display,
+			input pushbutton //may be used as clock
+			);
+
+reg [15:0] data_all;
+wire [3:0] keyout;
+reg [25:0] clk1;
+reg [1:0] ready_buffer;
+reg ack;
+reg statusordata;
+
+//memory map is defined here
+localparam	BEGINMEM=12'h000,
+		ENDMEM=12'h1ff,
+		KEYPAD=12'h900,
+		SEVENSEG=12'hb00;
+//  memory chip
+reg [15:0] memory [0:127]; 
  
-    input   pushbutton; 
-    output  [3:0] grounds;
-    output  [6:0] display;
-    input   clk; 
-    output led;
- 
-    //  memory chip
-    reg     [15:0] memory [0:127]; 
- 
-    // cpu's input-output pins
-    wire     [11:0] pc; //for debugging
-    wire     [15:0] data_out;
-    wire     [15:0] data_in;
-    wire     [11:0] address;
-    wire     memwt;
- 
-    wire     [15:0] reg0;
- 
-	assign led=memwt; //check memory write signal works or not. For debugging 
- 
-	//instantiation of cpu
-	reptile rr1 (.data_in(data_in), .data_out(data_out), .clk(clk), .memwt(memwt), .address(address),.reg0(reg0));
-    
-	//instantiation of seven segment
-	seven_segment_display ss1 (.grounds(grounds), .display(display), .clk(clk),  .data_in(reg0));
-    
- 
-	assign data_in=memory[address];
-    
-	 always @(posedge clk)
-		if (memwt==1)
+// cpu's input-output pins
+wire [15:0] data_out;
+reg [15:0] data_in;
+wire [11:0] address;
+wire memwt;
+
+
+sevensegment ss1 (//to be added);
+
+keypad  kp1(//to be added);
+
+bird br1 (//to be added);
+
+
+//multiplexer for cpu input
+always @*
+	if ( (BEGINMEM<=address) && (address<=ENDMEM) )
+		begin
+			data_in=memory[address];
+			ack=0;
+			statusordata=0;
+		end
+	else if (address==KEYPAD+1)
+		begin	
+			statusordata=1;
+			data_in=keyout;
+			ack=0;
+		end
+	else if (address==KEYPAD)
+		begin
+			//to be added 
+		end
+	else
+		begin
+			data_in=16'hf345; //any number
+			ack=0;
+			statusordata=0;
+		end
+
+//multiplexer for cpu output 
+
+always @(posedge clk) //data output port of the cpu
+	if (memwt)
+		if ( (BEGINMEM<=address) && (address<=ENDMEM) )
 			memory[address]<=data_out;
-	
-	
-    initial begin
+		else if ( SEVENSEG==address) 
+			data_all<=data_out;
+
+
+initial 
+	begin
+		data_all=0;
+		ack=0;
+		statusordata=0;
 		$readmemh("ram.dat", memory);
-    end
- 
+	end
+
 endmodule
-
-
