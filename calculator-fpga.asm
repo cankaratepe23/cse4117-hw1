36a6f5737ceb7a650ab98c730ae3b5f84d65de02
Can Karatepe <cankaratepe2000@gmail.com>
2022-01-07 21:08:40 +0300

    Add inital jump points to check for op buttons.

--- calculator-fpga.asm
+++ calculator-fpga.asm
@@ -1,17 +1,32 @@
 .data
                 zero: 0
+                // * = 0x000E
+                // # = 0x000F
 .code
                 push 0              // initialize stack
 start           call poll_and_read  // read a character
+
+                // reg0 is used for function return values
+                // reg5 is the current entered operand
+
                 mov 4 0             // put the new character into another register for safe-keeping
-                // check if # or * or A
-                mov 1 5             // multiply the previous number by 10
+                mov 1 0
+                ldi 2 0x000e
+                sub 0 1 2           // if entered key is 0xe
+                jz mul_op           // go to multiplication
+                ldi 2 0x000f
+                sub 0 1 2           // if entered key is 0xf
+                jz sum_op           // go to addition
+                //// default case, a new digit is entered:
+                mov 1 5             // prepare to mult the previous number by 10
                 ldi 2 10
                 call mult           // reg0 = reg5 * 10
-                add 0 0 4           // add the new number to the multiplied number
+                add 0 0 4           // add the new number to the multiplied old number
                 mov 5 0             // the current number resides in reg0 and reg5
                 call write_display
-                jmp start  // continue polling
+                jmp start           // continue polling
+sum_op          push 0              // placeholder
+mul_op          push 0              // placeholder         
 
 // mult: reg0 = reg1 * reg2
 // value of reg0 is NOT preserved
